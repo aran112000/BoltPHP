362c054da53771d4e4eba1be2d2f33c796ca0701
Aran Reeks <cdtreeks@hotmail.com>
2014-08-31 03:19:43 +0100

    Added PSR-0 Autoloader

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . '/Core/Init.php');
+error_reporting(E_ALL);
+spl_autoload_register(function($class_name) {
+	require($class_name . '.php');
+});
 
 $init = new \Core\Init();
