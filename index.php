283062d43720679db3dd6c80abc6cfcff779faff
Aran Reeks <cdtreeks@hotmail.com>
2014-12-28 15:11:27 +0000

    Updated our autoloader to prevent conflict issues with other libraries such as Twig when it was introduced

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-error_reporting(E_ALL);
 spl_autoload_register(function($class_name) {
-	require($class_name . '.php');
+	if (is_readable($class_name . '.php')) {
+		require($class_name . '.php');
+	}
 });
 
 $init = new \Core\Init();
