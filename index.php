0ea0b6fd88f9405312d592b08b48486d69a56c98
Aran Reeks <cdtreeks@hotmail.com>
2015-01-04 23:13:30 +0000

    Fixed autoloader paths to work with both Linux & Windows DS

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-spl_autoload_register(function($class_name) {
-	if (is_readable($class_name . '.php')) {
-		require($class_name . '.php');
-	}
+spl_autoload_register(function ($class_name) {
+    $class_path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class_name) . '.php';
+    if (is_readable($class_path)) {
+        require($class_path);
+    }
 });
 
 $init = new \Bolt\Init();
