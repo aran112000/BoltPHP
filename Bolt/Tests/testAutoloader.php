e8b1cc80a126cf95855cff3433d0e58b0e092bdb
Aran Reeks <cdtreeks@hotmail.com>
2015-01-04 23:06:31 +0000

    Improved the DOCUMENT_ROOT path generation

--- Bolt/Tests/testAutoloader.php
+++ Bolt/Tests/testAutoloader.php
@@ -1,6 +1,22 @@
 <?php
 define('BACKGROUND_PROCESS', true);
-$_SERVER['DOCUMENT_ROOT'] = 'E:\Bolt';
+if (empty($_SERVER['DOCUMENT_ROOT'])) {
+    // For environments where the DOCUMENT_ROOT isn't set, we generate it from the current file path
+    $path_parts = explode(DIRECTORY_SEPARATOR, __FILE__);
+    $path_parts = array_reverse($path_parts);
+    foreach ($path_parts as $key => $path) {
+        if ($path !== 'Bolt') {
+            unset($path_parts[$key]);
+        } else {
+            unset($path_parts[$key]);
+            break;
+        }
+    }
+
+    $_SERVER['DOCUMENT_ROOT'] = implode(DIRECTORY_SEPARATOR, array_reverse($path_parts));
+}
+
+echo '<p><pre>' . print_r($_SERVER, true) . '</pre></p>';
 
 spl_autoload_register(function($class_name) {
 	if (is_readable($class_name . '.php')) {
