b583b3e6b3e03962b65c5aa24ebace26203c1258
Aran Reeks <cdtreeks@gmail.com>
2015-01-10 13:11:47 +0000

    Minor code cleanup

--- Bolt/Exception/Exception.php
+++ Bolt/Exception/Exception.php
@@ -13,7 +13,7 @@ class Exception extends \Exception {
      * @param \Exception $previous
      */
     public function __construct($message = '', $code = 0, \Exception $previous = null) {
-		parent::__construct($message, $code,  $previous);
+		parent::__construct($message, $code, $previous);
 
 		$this->do_log_error($message);
 	}
