dbbfc46b530207a03b8587915ac2a1904a7671f8
Aran Reeks <cdtreeks@hotmail.com>
2015-01-10 21:24:13 +0000

    Refactored getSql() breaking out each piece of the SQL generation to remove code complexity

--- Bolt/Database/Mysql.php
+++ Bolt/Database/Mysql.php
@@ -94,103 +94,174 @@ class Mysql extends Database {
 		return $result_set;
 	}
 
+    /**
+     * @return string
+     */
+    protected function getSqlSelectString() {
+        $selects = [];
+        foreach($this->select as $select) {
+            if (!strstr($select, '.')) {
+                // If no derived table name has been specified, then fallback to using the main (FROM) table name
+                $selects[] = '`' . $this->from . '`.`' . $select . '`';
+            } else {
+                $selects[] = '`' . str_replace('.', '`.`', $select) . '`';
+            }
+        }
+
+        return 'SELECT ' . ($this->format ? "\n\t" : '') . (!empty($selects) ? implode(($this->format ? "\n\t" : '') . ',', $selects) : '`' . $this->from . '`.*');
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSqlFromString() {
+        return 'FROM ' . ($this->format ? "\n\t" : '') . '`' . $this->from . '`';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSqlWhereString() {
+        if (!empty($this->where)) {
+            $wheres = [];
+            foreach($this->where as $field => $details) {
+                if (!strstr($field, '.')) {
+                    $field = '`' . $this->from . '`.`' . $field . '`';
+                } else {
+                    $field = '`' . str_replace('.', '`.`', $field) . '`';
+                }
+                if ($details['raw']) {
+                    $wheres[] = $field . $details['value'];
+                } else {
+                    $wheres[] = $field . ($this->format ? ' ' : '') . $details['operator'] . ($this->format ? ' ' : '') . $details['value'];
+                }
+            }
+            return 'WHERE ' . ($this->format ? "\n\t" : '') . implode(' AND ' . ($this->format ? "\n\t" : '') . '', $wheres);
+        }
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSqlJoinString() {
+        if (!empty($this->join)) {
+            $joins = [];
+            foreach($this->join as $table => $details) {
+                $joins[] = strtoupper($details['type']) . ' JOIN `' . $table . '` ON ' . $details['condition'];
+            }
+
+            return implode(' ', $joins);
+        }
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSqlGroupByString() {
+        if (!empty($this->group_by)) {
+            $group_by = [];
+            foreach($this->group_by as $group) {
+                if (!strstr($group, '.')) {
+                    // If no derived table name has been specified, then fallback to using the main (FROM) table name
+                    $group_by[] = '`' . $this->from . '`.`' . $group . '`';
+                } else {
+                    $group_by[] = '`' . str_replace('.', '`.`', $group) . '`';
+                }
+            }
+
+            return 'GROUP BY ' . ($this->format ? "\n\t" : '') . implode(($this->format ? "\n\t" : '') . ',', $group_by);
+        }
+
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSqlHavingString() {
+        if (!empty($this->having)) {
+            $having = [];
+            foreach($this->having as $hav) {
+                if (!strstr($hav, '.')) {
+                    // If no derived table name has been specified, then fallback to using the main (FROM) table name
+                    $having[] = '`' . $this->from . '`.`' . $hav . '`';
+                } else {
+                    $having[] = '`' . str_replace('.', '`.`', $hav) . '`';
+                }
+            }
+
+            return 'HAVING ' . ($this->format ? "\n\t" : '') . implode(($this->format ? "\n\t" : '') . ',', $having);
+        }
+
+        return '';
+    }
+
+
+    /**
+     * @return string
+     */
+    protected function getSqlOrderByString() {
+        if (!empty($this->order_by)) {
+            $order_bys = [];
+            foreach($this->order_by as $order_by) {
+                if (!strstr($order_by, '.')) {
+                    // If no derived table name has been specified, then fallback to using the main (FROM) table name
+                    $order_bys[] = '`' . $this->from . '`.`' . $order_by . '`';
+                } else {
+                    $order_bys[] = '`' . str_replace('.', '`.`', $order_by) . '`';
+                }
+            }
+
+            return 'ORDER BY ' . ($this->format ? "\n\t" : '') . implode(($this->format ? "\n\t" : '') . ',', $order_bys);
+        }
+
+        return '';
+    }
+
+
+    /**
+     * @return string
+     */
+    protected function getSqlLimitString() {
+        if ($this->limit > 0) {
+            return 'LIMIT ' . (int) $this->limit;
+        }
+
+        return '';
+    }
+
 	/**
 	 * @return string
 	 */
 	public function getSql() {
-		$sql = [];
-		$selects = [];
-		foreach ($this->select as $select) {
-			if (!strstr($select, '.')) {
-				// If no derived table name has been specified, then fallback to using the main (FROM) table name
-				$selects[] = '`' . $this->from . '`.`' . $select . '`';
-			} else {
-				$selects[] = '`' . str_replace('.', '`.`', $select) . '`';
-			}
-		}
+        $sql_parts = [
+            $this->getSqlSelectString(),
+            $this->getSqlFromString(),
+            $this->getSqlWhereString(),
+            $this->getSqlJoinString(),
+            $this->getSqlGroupByString(),
+            $this->getSqlHavingString(),
+            $this->getSqlOrderByString(),
+            $this->getSqlLimitString(),
+        ];
 
-		$sql[] = 'SELECT ' . ($this->format ? "\n\t" : '') . (!empty($selects) ? implode(($this->format ? "\n\t" : '') . ',', $selects) : '`' . $this->from . '`.*');
-		$sql[] = 'FROM ' . ($this->format ? "\n\t" : '') . '`' . $this->from . '`';
+        $sql = '';
+        foreach ($sql_parts as $sql_part) {
+            if (!empty($sql_part)) {
+                if ($this->format) {
+                    $sql .= $sql_part . "\n";
+                } else {
+                    $sql .= ' ' . $sql_part;
+                }
+            }
+        }
+        $sql = trim($sql, "\n ");
+        $sql .= ';';
 
-		// Where clauses
-		if (!empty($this->where)) {
-			$wheres = [];
-			foreach ($this->where as $field => $details) {
-				if (!strstr($field, '.')) {
-					$field = '`' . $this->from . '`.`' . $field . '`';
-				} else {
-					$field = '`' . str_replace('.', '`.`', $field) . '`';
-				}
-				if ($details['raw']) {
-					$wheres[] = $field . $details['value'];
-				} else {
-					$wheres[] = $field . ($this->format ? ' ' : '') . $details['operator'] . ($this->format ? ' ' : '') . $details['value'];
-				}
-			}
-			$sql[] = 'WHERE ' . ($this->format ? "\n\t" : '') . implode(' AND ' . ($this->format ? "\n\t" : '') . '', $wheres);
-		}
-
-		// Join clauses
-		if (!empty($this->join)) {
-			foreach ($this->join as $table => $details) {
-				$sql[] = strtoupper($details['type']) . ' JOIN `' . $table . '` ON ' . $details['condition'];
-			}
-		}
-
-		// Group by clauses
-		if (!empty($this->group_by)) {
-			$group_by = [];
-			foreach ($this->group_by as $group) {
-				if (!strstr($group, '.')) {
-					// If no derived table name has been specified, then fallback to using the main (FROM) table name
-					$group_by[] = '`' . $this->from . '`.`' . $group . '`';
-				} else {
-					$group_by[] = '`' . str_replace('.', '`.`', $group) . '`';
-				}
-			}
-
-			$sql[] = 'GROUP BY ' . ($this->format ? "\n\t" : '') . implode(($this->format ? "\n\t" : '') . ',', $group_by);
-		}
-
-		// Having clauses
-		if (!empty($this->having)) {
-			$having = [];
-			foreach ($this->having as $hav) {
-				if (!strstr($hav, '.')) {
-					// If no derived table name has been specified, then fallback to using the main (FROM) table name
-					$having[] = '`' . $this->from . '`.`' . $hav . '`';
-				} else {
-					$having[] = '`' . str_replace('.', '`.`', $hav) . '`';
-				}
-			}
-
-			$sql[] = 'HAVING ' . ($this->format ? "\n\t" : '') . implode(($this->format ? "\n\t" : '') . ',', $having);
-		}
-
-		// Order by clauses
-		if (!empty($this->order_by)) {
-			$order_bys = [];
-			foreach ($this->order_by as $order_by) {
-				if (!strstr($order_by, '.')) {
-					// If no derived table name has been specified, then fallback to using the main (FROM) table name
-					$order_bys[] = '`' . $this->from . '`.`' . $order_by . '`';
-				} else {
-					$order_bys[] = '`' . str_replace('.', '`.`', $order_by) . '`';
-				}
-			}
-
-			$sql[] = 'ORDER BY ' . ($this->format ? "\n\t" : '') . implode(($this->format ? "\n\t" : '') . ',', $order_bys);
-		}
-
-		// Limit Clause
-		if ($this->limit > 0) {
-			$sql[] = 'LIMIT ' . (int) $this->limit;
-		}
-
-		if ($this->format) {
-			return implode("\n", $sql) . ';';
-		} else {
-			return implode(' ', $sql) . ';';
-		}
+        return $sql;
 	}
 }
